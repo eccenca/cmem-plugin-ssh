f9ccad6c3cc030a416eca4b603e152a4a32dbbbe
Louis Westerheide <louis.westerheide@eccenca.com>
2025-06-26 09:54:59 +0200

    first parameters

--- cmem_plugin_ssh/execute_commands.py
+++ cmem_plugin_ssh/execute_commands.py
@@ -1,1 +1,64 @@
 """Execute command task workflow plugin"""
+from cmem_plugin_base.dataintegration.description import Icon, PluginParameter
+from cmem_plugin_base.dataintegration.parameter.choice import ChoiceParameterType
+from cmem_plugin_base.dataintegration.parameter.password import PasswordParameterType
+
+from cmem_plugin_ssh.autocompletion import DirectoryParameterType
+from cmem_plugin_ssh.utils import AUTHENTICATION_CHOICES
+
+
+@Plugin(
+    label="Execute commands via SSH",
+    plugin_id="cmem_plugin_ssh-Execute",
+    description="Execute commands on a given SSH instance.",
+    documentation="""
+    """,
+    icon=Icon(package=__package__, file_name="ssh-icon.svg"),
+    parameters=[
+        PluginParameter(
+            name="hostname",
+            label="Hostname",
+            description="Hostname to connect to.Usually in the form of an IP address",
+        ),
+        PluginParameter(
+            name="port",
+            label="Port",
+            description="The port on which the connection will be tried on. Default is 22.",
+            default_value=22,
+        ),
+        PluginParameter(
+            name="username",
+            label="Username",
+            description="The username of which a connection will be instantiated.",
+        ),
+        PluginParameter(
+            name="authentication_method",
+            label="Authentication method",
+            description="The method that is used to connect to the SSH server.",
+            param_type=ChoiceParameterType(AUTHENTICATION_CHOICES),
+            default_value=AUTHENTICATION_CHOICES["password"],
+        ),
+        PluginParameter(
+            name="private_key",
+            label="Private key",
+            description="Your private key to connect via SSH.",
+            param_type=PasswordParameterType(),
+            default_value="",
+        ),
+        PluginParameter(
+            name="password",
+            label="Password",
+            description="Depending on your authentication method this will either be used to"
+            "connect via password to SSH or is used to decrypt the SSH private key",
+            param_type=PasswordParameterType(),
+            default_value="",
+        ),
+        PluginParameter(
+            name="path",
+            label="Path",
+            description="The currently selected path withing your SSH instance.",
+            default_value="",
+            param_type=DirectoryParameterType("directories", "Folder"),
+        ),
+    ],
+)
